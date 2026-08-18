a432bd4bb2d614cda4f61d228348917484cf110c
jadehawk <jadehawk@hotmail.com>
2026-08-18 09:30:52 -0400

    Bump plugin version to 0.2.3

--- libby-dashboard.koplugin/_meta.lua
+++ libby-dashboard.koplugin/_meta.lua
@@ -4,5 +4,5 @@ return {
     name = "libby-dashboard",
     fullname = _("Libby Dashboard"),
     description = _("Browse and download your Libby loans."),
-    version = "0.2.2",
+    version = "0.2.3",
 }
